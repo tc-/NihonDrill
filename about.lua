edb6a0ef442fbdca5912888fddbe798db08415fb
Tommy Carlsson <tommyc@lavabit.com>
2011-01-16 23:17:58 +0100

    Fireworks and website on the credits view

--- about.lua
+++ about.lua
@@ -13,6 +13,9 @@ local gui = require("gui")
 local status = nil
 local user = nil
 
+local parts = {}
+local next_explotion = 0
+
 function M.init(data)
 	status = data.status
 	user = data.user
@@ -20,8 +23,6 @@ end
 
 function M.show()
 end
-
-local parts = {}
 
 function M.mousepressed(x, y, button)
 	set_parts_pos(parts, x - 16, y - 16)
@@ -33,6 +34,11 @@ end
 
 function M.update(dt, mx, my)
 	gui.update_credits_parts(dt, parts, 24)
+	next_explotion = next_explotion - dt
+	if next_explotion < 0 then
+		next_explotion = math.random(8, 16) * 0.1
+		set_parts_pos(parts, math.random(0, lg.getWidth()) - 16, math.random(lg.getHeight()) - 16)
+	end
 end
 
 function M.draw()
@@ -49,6 +55,8 @@ function M.draw()
 	kana.draw_text("Other", 30, basey + 200, 70, color.credits_what, "tl")
 	kana.draw_text("Tuffy Font: Thatcher Ulrich (http://tulrich.com/fonts/)", 60, basey + 250, 50, color.credits_name, "tl")
 	kana.draw_text("Sound files: http://thejapanesepage.com/", 60, basey + 300, 50, color.credits_name, "tl")
+
+	kana.draw_text("Website: https://github.com/tc-/NihonDrill/wiki", 240, lg.getHeight() - 40, 40, color.credits_name, "tl")
 
 	b = gui.draw_back(status.button.name == "#back")
 	table.insert(status.buttons, b)
